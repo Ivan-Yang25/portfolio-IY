993d38407fa85da576778c895c1e21ca2b746e48
Ivan Yang <yangivan@gmail.com>
2024-02-22 18:36:17 -0300

    Modificacion de archivo

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="assets/img-portfolio.png">
+    <link rel="stylesheet" type="text/css" href="CSS/style.css" />
+    <title>Ivan Yang - Portfolio</title>
+</head>
+<body class="proyect__body">
+    <header class="body__header">
+        <h1 class="header__h1">
+            Ivan Yang - Porfolio
+        </h1>
+        <ul class="header__ul">
+            <li class="ul__li"><a href="#">About me</a><li>
+            <li class="ul__li"><a href="#">Proyects</a><li>
+            <li class="ul__li"><a href="#">Contact</a><li>
+            <li class="ul__li"><a href="#">Improvement options</a><li>
+            <li class="ul__li"><a href="#">Login</a></li>
+        </ul>
+    </header>
+    
+    <section class="body__section">
+        <h1 data-aos="fade-up">
+            ¡Wel<span class="visual">come</span> to<span class="visual">my</span> porfolio!
+        </h1>
+        <img data-aos="fade-up" src="assets/img-portfolio.png" alt="Imagen de robot programando" />
+    </section>
+    <section class="body__section">
+        <article class="section__article">
+
+        </article>
+    </section>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <script src="JS/main.js" />
+</body>
+</html>
